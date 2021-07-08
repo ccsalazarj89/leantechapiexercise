7fda95500c8e5f5ec28db5817efa2e71fad4c8e8
Christian Salazar <ccsalazarj@gmail.com>
2021-07-07 21:07:20 -0500

    it adds a import file for sql test row

---
+++ src/main/resources/import.sql
@@ -0,0 +1,18 @@
+insert into candidate (address,cellphone,city_name,last_name,name) values ('cra 123 fake' ,'13456','medellin','salazar','christian');
+insert into candidate (address,cellphone,city_name,last_name,name) values ('cra 123 fake' ,'13456','medellin','gonzalez','michelle');
+insert into candidate (address,cellphone,city_name,last_name,name) values ('cll 123 fake' ,'13477','medellin','usuga','alejandro');
+
+insert into candidate (address,cellphone,city_name,last_name,name) values ('cll 124 fake' ,'10077','cartagena','sponge','bob');
+
+insert into position (name) values ('Ingeniero');
+insert into position (name) values ('Repostero');
+insert into position (name) values ('Tecnico');
+
+insert into employee(salary, person_id,position_id) values (2000000,1,1);
+insert into employee(salary, person_id,position_id) values (1000000,2,2);
+insert into employee(salary, person_id,position_id) values (900000,3,3);
+insert into employee(salary, person_id,position_id) values (950000,4,2);
+
+
+
+
