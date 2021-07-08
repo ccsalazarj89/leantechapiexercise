70d255c1871fdd3c03c16a9e0a0f4df550fb278f
Christian Salazar <ccsalazarj@gmail.com>
2021-07-07 21:08:29 -0500

    it creates a test for the controller

---
+++ src/test/java/io/leanteach/pruebatecnica/app/controllers/EmployeeControllerTest.java
@@ -0,0 +1,39 @@
+package io.leanteach.pruebatecnica.app.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void listEmployee() {
+    }
+
+    @Test
+    void listPosition() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void save() {
+
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+}
